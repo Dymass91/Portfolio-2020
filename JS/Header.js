a8e21ae3870348b31fd6609796fb3a714a461885
Tomasz Matyszczak <Matyszczak24@gmail.com>
2019-10-29 20:46:19 +0000

    Fade in Navbar after scroll

--- JS/Header.js
+++ JS/Header.js
@@ -1,12 +1,33 @@
 const headerBg = document.getElementById('bg')
-const headercontent = document.getElementsByClassName('content')
-
+const navbar = document.getElementsByClassName('navbar')
 
 window.addEventListener('scroll', () => {
     headerBg.style.opacity = 1 - +window.pageYOffset / 550 + '';
     headerBg.style.top = +window.pageYOffset + 'px';
     headerBg.style.backgroundPositionY = - +window.pageYOffset / 2 + 'px';
 
-
 })
+
+// document.addEventListener('scroll', () => {
+//     if (document.body.scrollTop > 300) {
+//         navbar.addClass('show');
+//     }
+//     else {
+//         navbar.removeClass('show');
+//     };
+// });
+
+$(window).scroll(function () {
+    // 100 = The point you would like to fade the nav in.
+
+    if ($(window).scrollTop() > 300) {
+
+        $('.navbar').addClass('show');
+
+    } else {
+
+        $('.navbar').removeClass('show');
+
+    };
+});
 
