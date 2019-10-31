10e6a6887b4e09e8426cf3f8edcea97124a81ae8
Tomasz Matyszczak <Matyszczak24@gmail.com>
2019-10-31 11:07:57 +0000

    correction

--- JS/Header.js
+++ JS/Header.js
@@ -1,6 +1,5 @@
 const headerBg = document.getElementById('bg')
 
-
 window.addEventListener('scroll', () => {
     headerBg.style.opacity = 1 - +window.pageYOffset / 550 + '';
     headerBg.style.top = +window.pageYOffset + 'px';
